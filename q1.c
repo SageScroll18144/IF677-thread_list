5ac0fa38439a6da89234c17be4845ae7344c2073
GuilhermeMontt <113855003+GuilhermeMontt@users.noreply.github.com>
2023-08-24 22:59:21 -0300

    Update q1.c
    adicionando pthread_join

--- q1.c
+++ q1.c
@@ -7,7 +7,8 @@ int count;
 char decode[11];
 char senha[] = "aeiouAEIOU";
 
-typedef struct thread_id{
+typedef struct thread_code{
+    int thread_id;
     char comparador;
     int char_t;
 }T;
@@ -16,13 +17,13 @@ void *findChar(void *threadid){
     
     T tid = *((T *)threadid);
     int flag = 0, x = 33;
-    
+    tid.char_t = tid.thread_id;
     for(; tid.comparador || flag == 0;){
         tid.comparador = x;
         if(tid.comparador == senha[tid.char_t]){
             decode[tid.char_t] = tid.comparador;
             flag = 1;
-            printf("caracter %d achado\n\n", tid.char_t);
+            printf("caracter %d achado pela thread %d\n\n", tid.char_t, tid.thread_id);
             count++;
         }
         x++;
@@ -41,17 +42,21 @@ int main (int argc, char *argv[]){
     int t;   
 
     for(t=0; t<NUM_THREADS; t++){   
+        
         taskids[t] = (T *) malloc(sizeof(T)); 
-        taskids[t]->char_t = t;
-        int char_t = 33;
+        taskids[t]->thread_id = t;
+        
         rc = pthread_create(&threads[t], NULL, &findChar, (void *) taskids[t]);      
         if (rc){         
             printf("ERRO; código de retorno é %d\n", rc);         
             exit(-1);      
         }   
-    }  
+    }
     
-    for(int i=0; i<10000000; i++){}
+    for(t=0; t<NUM_THREADS; t++){
+        pthread_join(threads[t], NULL);
+    }
+    
   
    printf("%s\n", decode);
   
