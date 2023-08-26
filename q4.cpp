de4555f6204c0d04d7a9b191c665583a29fe0d3a
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-26 16:13:33 -0300

    implementando o algoritmo de solução

--- q4.cpp
+++ q4.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 #define ii pair<int, int>
 
-const int maxn = 1e3+10;
-ii ds[maxn][maxn];
+const int maxn = 1e3+10; //max map's size
+ii ds[maxn][maxn];//set of fathers
+int adj_x[] = {0,0,-1,1};
+int adj_y[] = {1,-1,0,0};
 
 void dsBuild(){
     for (int i = 0; i < maxn; i++){
@@ -34,7 +36,35 @@ void dsUnion(ii u, ii v){
     ds[v.first][v.second] = u;
 }
 
+void solve(int **map, int lim_x, int lim_y){
+    for (int x = 0; x < lim_x; x++){
+        for (int y = 0; y < lim_y; y++){
+            if(map[x][y]){ //se for terra
+                for(int adj = 0; adj < 4; adj++){
+                    int ceil_adj_x = x + adj_x[adj];
+                    int ceil_adj_y = y + adj_y[adj]; 
+
+                    if(ceil_adj_x >= 0 && ceil_adj_x < lim_x && ceil_adj_y >= 0 && ceil_adj_y < lim_y){
+                        if(map[ceil_adj_x][ceil_adj_y]){//se for terra
+                            ii me = dsFind(x,y);
+                            ii ceil_adj = dsFind(ceil_adj_x,ceil_adj_y);
+
+                            if(me.first != ceil_adj.first || me.second != ceil_adj.second){
+                                dsUnion(me, ceil_adj);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    
+}
+
 int main(){
+    int N;
+    int map[N][N];
 
     return 0;
 }
