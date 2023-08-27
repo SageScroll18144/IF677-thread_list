bfffa265ac810e59efa5dff49505bc76183055cc
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-27 11:29:08 -0300

    resolvendo o problema sem thread

--- q2.cpp
+++ q2.cpp
@@ -5,26 +5,43 @@ using namespace std;
 
 typedef struct{
     int ID;
-
-}process;
+    int a, b, c;
+}resource;
 
 const int maxn = 110;
 
-vector<process> graph[maxn];
+vector<int> graph[maxn];
 bool visited[maxn];
 
-bool DFS_find_cycle(process node){
-    visited[node.ID] = true;
-    for(auto son : graph[node.ID]){
-        if(!visited[son.ID]) DFS_find_cycle(son);
-        else if(visited[son.ID]) return true;
+bool DFS_find_cycle(int node){
+    visited[node] = true;
+    for(auto son : graph[node]){
+        if(!visited[son]) return DFS_find_cycle(son);
+        else if(visited[son]) return true;
     }
     return false;
 }
 
 int main(){
     
-    //talvez trocar o tipo do grafo, de int para p_thread 
+    int N, M, adj_node;
+    printf("Informe a quantidade de nós do grafo: ");
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++){
+        printf("Informe a quantidade de operações para o processo/recurso %d: ", i+1);
+        scanf("%d", &M);
+        for(int j = 0; j < M; j++){
+            printf("Este processo requisita ou este recurso tem posse (de): ");
+            scanf("%d", &adj_node);
+
+            graph[i].push_back(adj_node);
+        }
+    }
+
+    printf("Modelado o Grafo com Sucesso!\n\tBuscando deadlock...\n\n");
+
+    printf("%d\n", DFS_find_cycle(0));
 
     return 0;
 }
