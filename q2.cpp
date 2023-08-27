0a203b3600ccda5d3071078bab4a86e15022b8f7
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-27 12:07:29 -0300

    finalizado questao 2

--- q2.cpp
+++ q2.cpp
@@ -1,17 +1,19 @@
-#include <bits/stdc++.h>
+#include <vector>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace std;
-
-typedef struct{
-    int ID;
-    int a, b, c;
-}resource;
 
 const int maxn = 110;
 
 vector<int> graph[maxn];
 bool visited[maxn];
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+bool flag_ans = false;
 
 bool DFS_find_cycle(int node){
     visited[node] = true;
@@ -20,6 +22,21 @@ bool DFS_find_cycle(int node){
         else if(visited[son]) return true;
     }
     return false;
+}
+
+void *solve(void * args_){
+    int *node = (int *) args_;
+
+    pthread_mutex_lock(&mutex);
+
+    for(int i = 0; i < maxn; i++) visited[i] = false;
+    bool ans_tmp = DFS_find_cycle(*node);
+    flag_ans = (ans_tmp) ? ans_tmp : flag_ans;
+
+    pthread_mutex_unlock(&mutex);
+
+    pthread_exit(NULL);
+
 }
 
 int main(){
@@ -32,16 +49,35 @@ int main(){
         printf("Informe a quantidade de operações para o processo/recurso %d: ", i+1);
         scanf("%d", &M);
         for(int j = 0; j < M; j++){
-            printf("Este processo requisita ou este recurso tem posse (de): ");
+            printf("\n(Infome um valor entre [1,NUM NODES]).\tEste processo %d requisita ou este recurso tem posse (de): ", i+1);
             scanf("%d", &adj_node);
 
-            graph[i].push_back(adj_node);
+            graph[i].push_back(adj_node - 1);
         }
     }
 
-    printf("Modelado o Grafo com Sucesso!\n\tBuscando deadlock...\n\n");
+    printf("\nModelado o Grafo com Sucesso!\n\tBuscando deadlock...\n\n");
 
-    printf("%d\n", DFS_find_cycle(0));
+    pthread_t list_threads[N];
+    int t, rc;
+    int *idx_args;
+    for(t = 0; t < N; t++) {
+        idx_args = (int *) malloc(sizeof(int));
+        idx_args = &t;
+        
+        rc = pthread_create(&list_threads[t], NULL, solve, (void *)idx_args);
+        
+        if(rc){
+            printf("ERRO; código de retorno é %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    for(t = 0; t < N; t++) pthread_join(list_threads[t], NULL);
+
+    printf("%s\n", (flag_ans) ? "\n - Deadlock encontrado." : "\n - Não foi encontrado Deadlock.");
+
+    pthread_exit(NULL);
 
     return 0;
 }
