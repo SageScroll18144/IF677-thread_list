4200d786456cbc3464b7b73ff1ea383dffe23743
Vitor Manoel <114445111+vitork15@users.noreply.github.com>
2023-08-29 01:06:07 -0300

    finalizando a questão (implementando dynamic e guided)

--- q6.cpp
+++ q6.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <pthread.h>
 #include <stack>
+#include <cmath>
 using namespace std;
 
 class for_parameters {
@@ -9,19 +10,14 @@ class for_parameters {
         void (*function)(int);
 };
 const int OMP_NUM_THREADS = 4; // variavel global relativo ao numero de threads
-pthread_mutex_t request = PTHREAD_MUTEX_INITIALIZER; // mutex para o schedule dynamic/guided 
 pthread_mutex_t write_on_screen = PTHREAD_MUTEX_INITIALIZER; // mutex para escrita
-pthread_mutex_t acess_to_task[OMP_NUM_THREADS] = PTHREAD_MUTEX_INITIALIZER; // mutex para acesso ao vetor work
+bool task[OMP_NUM_THREADS]; // semaforo para as threads
+bool not_finished, wait; // variaveis auxiliares
 stack<for_parameters> work[OMP_NUM_THREADS]; // vetor de stacks contendo as iteracoes a serem realizadas por cada thread
 
 void funcao(int i) {
     // funcao exemplo
     cout << "Iteracao " << i+1;
-}
-
-int max(int a, int b) {
-    if(a>b) return a;
-    else return b;
 }
 
 void* for_thread_static(void * var) {
@@ -35,6 +31,26 @@ void* for_thread_static(void * var) {
             parameter.function(i);
             cout << " na thread " << thread_num << endl;
             pthread_mutex_unlock(&write_on_screen);
+        }
+    }
+    free(var);
+    pthread_exit(NULL);
+}
+
+void* for_thread_dynamic(void * var) {
+    int thread_num = *((int *)var), i;
+    for_parameters parameter;
+    while(not_finished) {
+        if(task[thread_num]) {
+            parameter = work[thread_num].top();
+            work[thread_num].pop();
+            for(i = parameter.start; i<=parameter.end; i += parameter.step) {
+                pthread_mutex_lock(&write_on_screen);
+                parameter.function(i);
+                cout << " na thread " << thread_num << endl;
+                pthread_mutex_unlock(&write_on_screen);
+            }
+            task[thread_num] = false;
         }
     }
     free(var);
@@ -66,13 +82,82 @@ void omp_for( int inicio , int passo , int final , int schedule , int chunk_size
                     exit(1);
                 }
             }
-        }
+        } break;
         case 1: {
-
-        }
+            int iteration = inicio, thread_num = 0, i;
+            not_finished = true; wait = false;
+            bool check;
+            for(int i = 0; i<OMP_NUM_THREADS; i++) {
+                int* num = (int *) malloc(sizeof(int));
+                *num = i;
+                error = pthread_create(&thread[i], NULL, for_thread_dynamic, (void*)num);
+                if(error != 0) {
+                    cout << "Erro na criacao de thread";
+                    exit(1);
+                }
+                access_to_task[i] = PTHREAD_MUTEX_INITIALIZER;
+                task[i] = false;
+            }
+            while(not_finished) {
+                if(wait) {
+                    check = true;
+                    for(i = 0; i<OMP_NUM_THREADS; i++) {
+                        check = check && !task[i];
+                    }
+                    if(check) {
+                        not_finished = false;
+                    }
+                }
+                else if(!task[thread_num]) {
+                    if(iteration+(chunk_size*passo)-1 >= final) temp = final-1;
+                    else temp = iteration+(chunk_size*passo)-1;
+                    for_parameters parameters = {iteration, temp, passo, f};
+                    iteration += chunk_size*passo;
+                    work[thread_num].push(parameters);
+                    task[thread_num] = true;
+                    if(iteration >= final) wait = true;
+                }
+                thread_num = (thread_num+1)%OMP_NUM_THREADS;
+            }
+        } break;
         case 2: {
-
-        }
+            int iteration = inicio, thread_num = 0, i, tasknum = chunk_size;
+            not_finished = true; wait = false;
+            bool check;
+            for(int i = 0; i<OMP_NUM_THREADS; i++) {
+                int* num = (int *) malloc(sizeof(int));
+                *num = i;
+                error = pthread_create(&thread[i], NULL, for_thread_dynamic, (void*)num);
+                if(error != 0) {
+                    cout << "Erro na criacao de thread";
+                    exit(1);
+                }
+                access_to_task[i] = PTHREAD_MUTEX_INITIALIZER;
+                task[i] = false;
+            }
+            while(not_finished) {
+                if(wait) {
+                    check = true;
+                    for(i = 0; i<OMP_NUM_THREADS; i++) {
+                        check = check && !task[i];
+                    }
+                    if(check) {
+                        not_finished = false;
+                    }
+                }
+                else if(!task[thread_num]) {
+                    tasknum = max<int>(ceil((double)(final-iteration)/(double)OMP_NUM_THREADS), chunk_size);
+                    if(iteration+(tasknum*passo)-1 >= final) temp = final-1;
+                    else temp = iteration+(tasknum*passo)-1;
+                    for_parameters parameters = {iteration, temp, passo, f};
+                    iteration += tasknum*passo;
+                    work[thread_num].push(parameters);
+                    task[thread_num] = true;
+                    if(iteration >= final) wait = true;
+                }
+                thread_num = (thread_num+1)%OMP_NUM_THREADS;
+            }
+        } break;
     }
     for(int i = 0; i<OMP_NUM_THREADS; i++) { // espera ate todas as iteracoes acabarem
         pthread_join(thread[i], NULL);
@@ -89,7 +174,7 @@ int main() {
     {   
     f(i);
     }*/
-    int inicio = 0, passo = 1, final = 100, chunk_size = 10, schedule = 0; // schedule 0 = static, schedule 1 = dynamic, schedule 2 = guided
+    int inicio = 0, passo = 1, final = 15, chunk_size = 2, schedule = 0; // schedule 0 = static, schedule 1 = dynamic, schedule 2 = guided
     omp_for(inicio, passo, final, schedule, chunk_size, funcao);
     return 0;
 }
